9875aeb2fa6f8fafa6c9ffc776a8b970f194d067
Elizabeth Kleinschmidt <tenlia@hotmail.com>
2016-10-20 16:11:15 -0700

    added game-history docs

---
+++ lab-elizabeth/app/component/game-history/game-history.js
@@ -0,0 +1,19 @@
+'use strict';
+
+// require sass
+require('./_game-history.scss');
+
+const angular = require('angular');
+const textZ = angular.module('textZ');
+
+textZ.component('gameHistory', { // set the tag name to <game-history>
+  template: require('./game-history.html'),
+  controller: 'GameHistoryController',
+  controllerAs: 'gameHistoryCtrl',
+});
+
+textZ.controller('GameHistoryController', ['$log', 'playerService', GameHistoryController]);
+function GameHistoryController($log, playerService){
+  $log.debug('init gameHistoryCtrl');
+  this.history = playerService.history;
+}
